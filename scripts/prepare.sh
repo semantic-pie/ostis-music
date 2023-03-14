0562aba53a80d4ecad57c649c00438598bb85702
Глеб <90149444+glebchanskiy@users.noreply.github.com>
2023-03-14 17:56:24 +0300

    fix: in case, that ostis founded just moving kb

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,12 +7,12 @@ BASE_DIR=`pwd`
 git clone https://github.com/semantic-pie/ostis-example-app
 
 if [ -e ostis-example-app ]; then
+    echo "ostis-example-app founded, moving kb "
+    rm -rf $BASE_DIR/ostis-example-app/music-kb
+    cp -r $BASE_DIR/kb $BASE_DIR/ostis-example-app/kb/music-kb
+else
     cd ostis-example-app/scripts
     ./install_ostis.sh
     echo "Moving music-kb to ostis-exemaple-app kb"
-    cp -r $BASE_DIR/kb $BASE_DIR/ostis-example-app/kb/music-kb
-else
-    echo "ostis-example-app founded, moving kb "
-    rm -rf $BASE_DIR/ostis-example-app/music-kb
     cp -r $BASE_DIR/kb $BASE_DIR/ostis-example-app/kb/music-kb
 fi
