309d2ee46173b1aa7b4fdb6e3ed04c1457c9e55c
Levkov Gleb <Glebchanskiy@yandex.by>
2023-03-28 13:23:15 +0300

    fix: unnessesary git clone moved / naming fix

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,16 +1,16 @@
-!#/bin/bash
+#!/bin/bash
 
 cd ..
 
 BASE_DIR=`pwd`
 
-git clone https://github.com/semantic-pie/ostis-example-app
 
 if [ -e ostis-example-app ]; then
     echo "ostis-example-app founded, moving kb "
-    rm -rf $BASE_DIR/ostis-example-app/music-kb
+    rm -rf $BASE_DIR/ostis-example-app/kb/music-kb
     cp -r $BASE_DIR/kb $BASE_DIR/ostis-example-app/kb/music-kb
 else
+    git clone https://github.com/semantic-pie/ostis-example-app
     cd ostis-example-app/scripts
     ./install_ostis.sh
     echo "Moving music-kb to ostis-exemaple-app kb"
